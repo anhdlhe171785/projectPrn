ad79f01dc3b68c166dcf9fddf925476cccd398f9
truongtnhe170291 <truongtnhe170291@fpt.edu.vn>
2024-07-05 00:02:30 +0700

    update code 5/7

--- Group2WPF/MainWindow.xaml.cs
+++ Group2WPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using DataAccess;
+using LiveCharts;
+using LiveCharts.Wpf;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,15 +20,88 @@ namespace Group2WPF
 	public partial class MainWindow : Window
 	{
 		private FlightManagementDbContext _context;
+		public SeriesCollection SeriesCollection { get; set; }
+		public string[] Labels { get; set; }
+		public Func<double, string> YFormatter { get; set; }
 		public MainWindow()
 		{
 			InitializeComponent();
 			_context = new FlightManagementDbContext();
+			SeriesCollection = new SeriesCollection
+			{
+				new LineSeries
+				{
+					Title = "Today",
+					Values = new ChartValues<double> { 
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Now, 0, 0),
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Now, 0, 6), 
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Now, 6, 12), 
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Now, 12, 18),
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Now, 18, 24) }
+					//Values = new ChartValues<double> { CountRowsWithinTimeRange(_context.Bookings.ToList(), new DateTime(2022, 10, 11), 0, 0), CountRowsWithinTimeRange(_context.Bookings.ToList(), new DateTime(2022, 10, 11), 0, 6), CountRowsWithinTimeRange(_context.Bookings.ToList(), new DateTime(2022, 10, 11), 6, 12), CountRowsWithinTimeRange(_context.Bookings.ToList(), new DateTime(2022, 10, 11), 12, 18), CountRowsWithinTimeRange(_context.Bookings.ToList(), new DateTime(2022, 10, 11), 18, 24) }
+				},
+				new LineSeries
+				{
+					Title = "Yesterday",
+					Values = new ChartValues<double> { 
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 0, 0),
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 0, 6), 
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 6, 12),
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 12, 18),
+						CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 18, 24) },
+					//Values = new ChartValues<double> { CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 0, 0), CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 0, 6), CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 6, 12), CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 12, 18), CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 18, 24) },
+					PointGeometry = null
+				},
+				
+			};
+			double percen = CalculatePercentageIncrease(CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Now, 0, 24), CountRowsWithinTimeRange(_context.Bookings.ToList(), DateTime.Today.AddDays(-1), 0, 24));
+			if(percen >= 0)
+			{
+				lblpercen.Content = $"Increased {percen}% compared to yesterday";
+				lblpercen.Background = new SolidColorBrush(Colors.Green);
+			}
+			else
+			{
+				lblpercen.Content = $"Decrease {percen}% compared to yesterday";
+				lblpercen.Background = new SolidColorBrush(Colors.Red);
+			}
+			Labels = new[] { "0h", "6h", "12h", "16h", "24h" };
+			YFormatter = value => value.ToString("N0");
+
+
+			DataContext = this;
+		}
+		private int CountRowsWithinTimeRange(List<Booking> data, DateTime date, int startHour, int endHour)
+		{
+			var today = date;
+
+			var startTime = today.AddHours(startHour);
+			var endTime = today.AddHours(endHour);
+			if (endHour == 24)
+			{
+				endTime = endTime.AddMilliseconds(-1); // 23:59:59.999 của ngày đó
+			}
+			return data.Count(x => x.BookingTime >= startTime && x.BookingTime <= endTime);
+		}
+		private double CalculatePercentageIncrease(int todayCount, int yesterdayCount)
+		{
+			if (yesterdayCount == 0)
+			{
+				return todayCount > 0 ? 100.0 : 0; 
+			}
+
+			return ((double)(todayCount - yesterdayCount) / yesterdayCount) * 100;
+		}
+		private void btnPassenger_Click(object sender, RoutedEventArgs e)
+		{
+			PassengerWPF passengerWPF = new PassengerWPF();
+			passengerWPF.ShowDialog();
 		}
 
-		private void btn_Click(object sender, RoutedEventArgs e)
+		private void Flight_Click(object sender, RoutedEventArgs e)
 		{
-			lbl.Content = _context.Airlines.First().Name;
+			FlightWPF flightWPF = new FlightWPF();
+			flightWPF.ShowDialog();
 		}
 	}
 }
